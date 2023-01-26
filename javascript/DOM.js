4e0a62ff1fadbb41ff2800c9301d280c2704edff
Leogutierrez24 <leoalegutierrez97@gmail.com>
2023-01-26 18:00:52 -0300

    finished with the mobile menu, keep with the responsive design

--- javascript/DOM.js
+++ javascript/DOM.js
@@ -1,1 +1,4 @@
 const menuButton = document.getElementById("mobileButton");
+const mobileMenu = document.querySelector(".mobileMenu");
+const mobileLinks = document.querySelectorAll(".mml");
+const closeButton = document.querySelector(".mobileMenu-closeIcon");
--- javascript/index.js
+++ javascript/index.js
@@ -1,8 +1,23 @@
 let windowWidth = window.innerWidth;
 
 const openMenu = () => {
-    if (windowWidth <= 768) console.log("abrir menu")
+    if (windowWidth <= 768) {
+        mobileMenu.classList.add("opened");
+    }
+}
+
+const closeMenu = (target, element) => {
+    if (target === element) mobileMenu.classList.remove("opened");
 }
 
 window.addEventListener("resize", () => windowWidth = window.innerWidth);
+
 menuButton.addEventListener("click", openMenu);
+
+mobileMenu.addEventListener("click", ({ target }) => closeMenu(target, mobileMenu));
+
+mobileLinks.forEach((element) => {
+    element.addEventListener("click", ({ target }) => closeMenu(target, element));
+});
+
+closeButton.addEventListener("click", ({ target }) => closeMenu(target, closeButton));
